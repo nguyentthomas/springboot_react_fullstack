fd11e74713ac32094702539708fd3a9bbe353297
Thomas Nguyen <12613749@student.uts.edu.au>
2021-06-23 11:17:29 +1000

    changes to submenu

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -181,9 +181,9 @@ function App() {
                     Option 2
                 </Menu.Item>
                 <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
+                    <Menu.Item key="3">User1</Menu.Item>
+                    <Menu.Item key="4">User2</Menu.Item>
+                    <Menu.Item key="5">User3</Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
                     <Menu.Item key="6">Team 1</Menu.Item>
